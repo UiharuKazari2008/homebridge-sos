220278d1a6ae38d62d7a05dc3567295d571d1c17
Yukimi Kazari <dev@9854.moe>
2021-02-19 14:54:50 -0500

    Tring to get it to work

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ class HBSOS {
         });
     };
 
-    this.setItem = (uuid, value) => {
+    this.setValue = (uuid, value) => {
       MotionService
         .getCharacteristic(Characteristics[uuid])
         .setValue(value);
@@ -243,7 +243,7 @@ class HBSOS {
                 console.log(results);
                 if (results && results.content.value.item === req.query.value) {
                   res.status(200).send('OK');
-                  this.setItem(uuid, results.content.value.item);
+                  this.setValue(uuid, results.content.value.item);
                   this.triggerMotionEvent();
                   debug(`Save: "${results.content.key}" = "${results.content.value}"`);
                 } else {
