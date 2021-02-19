bd0daa8e6f13d0cf0e3a0029f793b955e145eb70
Yukimi Kazari <dev@9854.moe>
2021-02-19 11:22:42 -0500

    Init Commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const debug = require('debug')('homebridge-sos');
+const ACRSOS = require('./sos-server');
+
+let Service;
+let Characteristic;
+let CustomServer;
+
+class HBSOS {
+  constructor(log, config) {
+    this.log = log;
+    this.name = config.name;
+
+    this.dataDir = config.dataDir || './data';
+
+    this.httpport = config.httpPort || 8055;
+
+    this.tlsport = config.tlsPort || 9045;
+    this.tlscert = config.tlsCert;
+    this.tlskey = config.tlsKey;
+
+    this.contactCall = config.contactCall;
+
+    let TLSOptions;
+
+    if (this.tlscert && this.tlskey && this.tlscert.length > 2 && this.tlskey.length > 2) {
+      debug('TLS Support Enabled!');
+      TLSOptions = {
+        httpsPort: this.tlsport,
+        tlsCert: this.tlscert,
+        tlsKey: this.tlskey,
+      };
+    }
+
+    this.motionService = new Service.MotionSensor(this.name);
+
+    this.CustomServer = ACRSOS({
+      httpPort: this.httpport,
+      dataDir: this.dataDir,
+    }, TLSOptions);
+
+    this.contactCall.forEach((contact) => {
+      const state = CustomServer.getItem(contact);
+      this.service = new Service.ContactSensor(state.key);
+      this.service
+        .getCharacteristic(Characteristic.ContactSensorState)
+        .on('get', () => (state.value === 'Yes' || state.value === '1'));
+    });
+
+    this.addCharacteristics();
+    this.refreshValues();
+  }
+
+  refreshValues() {
+    CustomServer.getAllItems.forEach((object) => {
+      this.motionService
+        .getCharacteristic(() => {
+          const char = new Characteristic(object.key, object.uuid);
+
+          char.setProps({
+            format: Characteristic.Formats.UINT8,
+            maxValue: 12,
+            minValue: 1,
+            minStep: 1,
+            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY],
+          });
+          char.value = object.value;
+
+          return char;
+        })
+        .updateValue(CustomServer.getItem(object.key));
+    });
+    // Set timeout
+    setTimeout(
+      this.refreshValues.bind(this),
+      60000,
+    );
+  }
+
+  addCharacteristics() {
+    this.CustomServer.getAllItems.forEach((object) => {
+      this.motionService
+        .addCharacteristic(() => {
+          const char = new Characteristic(object.key, object.uuid);
+
+          char.setProps({
+            format: Characteristic.Formats.UINT8,
+            maxValue: 12,
+            minValue: 1,
+            minStep: 1,
+            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY],
+          });
+          char.value = object.value;
+
+          return char;
+        })
+        .on('get', callback => callback(null, CustomServer.getItem(object.key)));
+    });
+  }
+}
+
+module.exports = (homebridge) => {
+  Service = homebridge.hap.Service; // eslint-disable-line
+  Characteristic = homebridge.hap.Characteristic; // eslint-disable-line
+
+  homebridge.registerAccessory('homebridge-sos', 'ACRSOS', HBSOS);
+};
---
+++ sos-server.js
@@ -0,0 +1,256 @@
+/* eslint-disable max-len */
+const debug = require('debug')('homebridge-sos');
+const fs = require('fs');
+const storageHandler = require('node-persist');
+const express = require('express');
+const request = require('request').defaults({ encoding: null });
+const _http = require('http');
+const _https = require('https');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+const server = (Options, TLSOpts) => {
+  let https;
+
+  const http = _http.createServer(app).listen(Options.httpPort);
+  if (TLSOpts && TLSOpts.httpsPort) {
+    https = _https.createServer({
+      key: fs.readFileSync(TLSOpts.tlsKey),
+      cert: fs.readFileSync(TLSOpts.tlsCert),
+    }, app).listen(TLSOpts.httpsPort);
+  }
+
+  const storage = storageHandler.create({
+    dir: Options.dataDir,
+    stringify: JSON.stringify,
+    parse: JSON.parse,
+    encoding: 'utf8',
+    logging: false,
+    ttl: false,
+    expiredInterval: 2 * 60 * 1000, // every 2 minutes the process will clean-up the expired cache
+    forgiveParseErrors: true,
+  });
+
+  storage.init().then((res) => {
+    debug(`Initialized successfully the Local parameters storage @ ${res.dir}`);
+    storage.keys()
+      .then((keys) => {
+        debug('Values in storage:');
+        keys.forEach((key) => {
+          storage.get(key)
+            .then((value) => {
+              debug(`"${key}" = "${value}"`);
+            })
+            .catch((err) => {
+              debug(`Could not read ${key}'s value!`);
+            });
+        });
+      });
+  });
+
+  app.use(express.json({ limit: '5mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-WSS-Key, X-API-Key, X-User-Agent, User-Agent');
+    next();
+  });
+  app.get('/', (req, res) => {
+    res.status(200).send('<b>Lizumi Storage API v1.2</b>');
+  });
+  app.get('/get', (req, res) => {
+    if (req.query.item !== undefined && req.query.item !== '') {
+      storage.getItem(req.query.item)
+        .then((results) => {
+          if (results && results !== 'undefined' && results !== '') {
+            res.status(200).send(results);
+            debug(`Request: "${req.query.item}" = "${results}"`);
+          } else {
+            res.status(404).send('NOT_FOUND');
+            debug(`Request Error: "${req.query.item}" does not exist yet`);
+          }
+        })
+        .catch((err) => {
+          res.status(500).send();
+          debug(`Request Error: "${req.query.item}"`);
+          debug(err);
+        });
+    } else {
+      res.status(500).send('INVALID_REQUEST');
+      debug('Request Error missing query!');
+    }
+  });
+  app.get('/all', (req, res) => {
+    storage.keys()
+      .then((keys) => {
+        if (keys.length === 0) {
+          res.status(404).send('EMPTY');
+          debug('Request Error: There are no items in storage');
+        } else {
+          const results = [];
+          keys.forEach((key, index) => {
+            storage.get(key)
+              .then((value) => {
+                results.push({ key, value });
+                if (index === keys.length - 1) {
+                  res.status(200).send(results);
+                  debug(results);
+                }
+              })
+              .catch((err) => {
+                debug(`Could not read ${key}'s value!`);
+              });
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send();
+        debug('Request Error:');
+        debug(err);
+      });
+  });
+  app.get('/set', (req, res) => {
+    if (req.query.item !== undefined && req.query.value !== undefined && req.query.item !== '' && req.query.value !== '') {
+      let uuid;
+      storage.getItem(req.query.item)
+        .then((results) => {
+          if (results && results !== 'undefined' && results !== '') {
+            uuid = results.uuid;
+          } else {
+            uuid = uuidv4();
+          }
+        });
+      storage.setItem(req.query.item, {
+        item: req.query.value,
+        uuid,
+      })
+        .then((results) => {
+          if (results && results.content.value === req.query.value) {
+            res.status(200).send('OK');
+            debug(`Save: "${results.content.key}" = "${results.content.value}"`);
+          } else {
+            res.status(500).send('SAVE_FAILED');
+            debug(`Save Error: "${req.query.item}" did not save correctly`);
+          }
+        })
+        .catch((err) => {
+          res.status(500).send();
+          debug(`Save Error: "${req.query.item}" = "${req.query.value}"`);
+          debug(err);
+        });
+    } else {
+      res.status(500).send('INVALID_REQUEST');
+      debug('Save Error missing query or value!');
+    }
+  });
+  app.get('/del', (req, res) => {
+    if (req.query.item !== undefined && req.query.item !== '') {
+      storage.removeItem(req.query.item)
+        .then((results) => {
+          if (results && results.removed) {
+            res.status(200).send('OK');
+            debug(results);
+          } else if (results && results.existed === false) {
+            res.status(200).send('OK');
+            debug(results);
+          } else {
+            res.status(500).send('DELETE_FAILED');
+            debug(`Delete Error: "${req.query.item}" was not removed`);
+          }
+        })
+        .catch((err) => {
+          res.status(500).send();
+          debug(`Delete Error: "${req.query.item}" = "${req.query.value}"`);
+          debug(err);
+        });
+    } else {
+      res.status(500).send('INVALID_REQUEST');
+      debug('Delete Error missing query!');
+    }
+  });
+
+  /* app.get('/cet', (req, res) => {
+    if (req.query.item !== undefined && req.query.value !== undefined && req.query.item !== '' && req.query.value !== '') {
+      storage.setItem(req.query.item, req.query.value)
+        .then((results) => {
+          if (results && results.content.value === req.query.value) {
+            res.status(200).send('OK');
+            debug(`Save: "${results.content.key}" = "${results.content.value}"`);
+            if (req.query.opt !== undefined && req.query.opt !== '' && config['call-urls'][parseInt(req.query.opt.toString())] !== undefined && config['call-urls'][parseInt(req.query.opt.toString())] !== '') {
+              request({
+                url: config['call-urls'][parseInt(req.query.opt.toString())].toString(),
+                method: 'GET',
+                timeout: 5000,
+              }, (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                  debug(`Call: "${config['call-urls'][parseInt(req.query.opt.toString())].toString()}" = "${body}"`);
+                } else {
+                  debug(`Failed Call: "${config['call-urls'][parseInt(req.query.opt.toString())].toString()}" = "${body}"`);
+                }
+              });
+            } else {
+              debug('Failed Call: Unable to process the requested option');
+            }
+          } else {
+            res.status(500).send('SAVE_FAILED');
+            debug(`Save Error: "${req.query.item}" did not save correctly`);
+          }
+        })
+        .catch((err) => {
+          res.status(500).send();
+          debug(`Save Error: "${req.query.item}" = "${req.query.value}"`);
+          debug(err);
+        });
+    } else {
+      res.status(500).send('INVALID_REQUEST');
+      debug('Save Error missing query or value!');
+    }
+  }); */
+
+  const getAllItems = () => {
+    storage.keys()
+      // eslint-disable-next-line consistent-return
+      .then((keys) => {
+        if (keys.length === 0) {
+          return [];
+        }
+        const results = [];
+        keys.forEach((key, index) => {
+          storage.get(key)
+            // eslint-disable-next-line consistent-return
+            .then((value) => {
+              results.push({ key, value: value.item, uuid: value.uuid });
+              if (index === keys.length - 1) {
+                return results;
+              }
+            })
+            .catch((err) => {
+              debug(err.message);
+              return [];
+            });
+        });
+      })
+      .catch((err) => {
+        debug(err.message);
+        return [];
+      });
+  };
+
+  const getItem = (key) => {
+    storage.get(key)
+      // eslint-disable-next-line consistent-return
+      .then(value => (value.item))
+      .catch((err) => {
+        debug(err.message);
+        return 'NOVAL';
+      });
+  };
+
+  return {
+    getAllItems,
+    getItem,
+  };
+};
+
+module.exports = server;
